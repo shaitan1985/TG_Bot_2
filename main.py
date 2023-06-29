613a718bbbc059a356195420be81ae2e3e0fb2dc
K.Shaitanov <atray@mail.ru>
2023-06-29 18:16:25 +0300

    config refactored

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import asyncio
+
 from aiogram import Bot, Dispatcher
-from aiogram.types import Message
 from bot.handlers import messages, commands
-import config
+from config import Config, load_config
 
 
-# Bot object and dispatcher
-bot: Bot = Bot(token=config.TELEGRAM_BOT_TOKEN)
-dp: Dispatcher = Dispatcher()
+async def main() -> None:
+    # Bot object and dispatcher
+    config: Config = load_config('.env')
+    bot: Bot = Bot(token=config.tg_bot.token)
+    dp: Dispatcher = Dispatcher()
+
+    #это надо утащить в стартовый сетап
+    messages.setup(dp=dp)
+    commands.setup(dp=dp)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == '__main__':
-    messages.setup(dp=dp)
-    commands.setup(dp=dp)
-    dp.run_polling(bot)
+    asyncio.run(main())
